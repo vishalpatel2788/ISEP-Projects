ac69cf577e5d4612ada2a18cab64ed39670bba60
R-Aniruddha <aniruddha04@gmail.com>
2020-05-25 14:35:26 +0200

    Update Project - Replaced Javascript Implementation of API communication with Java

---
+++ coronadb.sql
@@ -0,0 +1,54 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 25, 2020 at 02:32 PM
+-- Server version: 10.3.16-MariaDB
+-- PHP Version: 7.3.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `coronadb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `details`
+--
+
+CREATE TABLE `details` (
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `message` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `details`
+--
+
+INSERT INTO `details` (`name`, `mobile`, `email`, `message`) VALUES
+('Ani', '9876543210', 'ani@email.com', 'This is a test.'),
+('John Snow', '987654321', 'john@email.com', 'This is an insertion test'),
+('Ani', '98765432', 'ani@email.com', 'TESTING'),
+('Aniruddha ', '764061654', 'anir@email.com', 'asdfasdfsdf'),
+('Aniruddha ', '764061654', 'anir@email.com', 'asdfasdfsdf'),
+('Rakesh', '84654165', 'rakesh@email.com', 'Another test'),
+('Rakesh Nuvvula', '84654165', 'rakesh@email.com', 'Another test 123');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
